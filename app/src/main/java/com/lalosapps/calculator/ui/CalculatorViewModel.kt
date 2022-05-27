4aae0128cf120f9d09f684c3ad3830c6d827590b
cbarrios <lalobarrios16@yahoo.com>
2022-05-27 11:58:39 -0400

    master: (11) view model onAction function

--- app/src/main/java/com/lalosapps/calculator/ui/CalculatorViewModel.kt
+++ app/src/main/java/com/lalosapps/calculator/ui/CalculatorViewModel.kt
@@ -4,9 +4,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.lalosapps.calculator.core.CalculatorAction
 
 class CalculatorViewModel : ViewModel() {
-    
+
     var state by mutableStateOf(CalculatorState())
         private set
+
+    fun onAction(action: CalculatorAction) {
+        when (action) {
+            is CalculatorAction.Clear -> clearState()
+            is CalculatorAction.Delete -> performDeletion()
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Calculate -> performOperation()
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+        }
+    }
 }
