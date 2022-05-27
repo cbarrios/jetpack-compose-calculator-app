b28658a14e36ffb71389b0040895e6f240230ad4
cbarrios <lalobarrios16@yahoo.com>
2022-05-27 10:55:33 -0400

    master: (7) get state from view model in main activity

--- app/src/main/java/com/lalosapps/calculator/MainActivity.kt
+++ app/src/main/java/com/lalosapps/calculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.lalosapps.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.lalosapps.calculator.ui.CalculatorViewModel
 import com.lalosapps.calculator.ui.theme.CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +12,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorTheme {
-
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
             }
         }
     }
